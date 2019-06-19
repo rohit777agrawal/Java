5995714b860ba12b05e59006c2089b815663fa41
Rohit Agrawal <36775165+rohit777agrawal@users.noreply.github.com>
2019-06-19 10:30:14 -0400

    Add files via upload

---
+++ EggBasket.java
@@ -0,0 +1,19 @@
+public class EggBasket
+{
+    public static void main(String[] args)
+    {
+        int numberOfBaskets;
+        int eggsPerBasket;
+        int totalEggs;
+
+        numberOfBaskets = 10;
+        eggsPerBasket = 6;
+
+        totalEggs = numberOfBaskets * eggsPerBasket;
+
+        System.out.println("If you have");
+        System.out.println(eggsPerBasket + " eggs per basket and");
+        System.out.println(numberOfBaskets + " baskets, then");
+        System.out.println("the total number of eggs is " + totalEggs);
+    }
+}
